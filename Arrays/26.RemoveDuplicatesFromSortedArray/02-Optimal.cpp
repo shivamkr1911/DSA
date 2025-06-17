4c48531c4bf9660b641e8355fe8653cb73f85594
Shivam <shivamkrsingh1911@gmail.com>
2025-06-18 00:03:33 +0530

    Added LeetCode 26

---
+++ Arrays/26.RemoveDuplicatesFromSortedArray/02-Optimal.cpp
@@ -0,0 +1,22 @@
+// Optimal Approach
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution
+{
+public:
+    int removeDuplicates(vector<int> &nums)
+    {
+        // Optimal - Two Pointer
+        int i = 0;
+        for (int j = 1; j < nums.size(); j++)
+        {
+            if (nums[i] != nums[j])
+            {
+                nums[i + 1] = nums[j];
+                i++;
+            }
+        }
+        return (i + 1);
+    }
+};
