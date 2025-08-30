ef2b5bc7de3334a96fed7efb93b0e9c4802448df
Shivam <shivamkrsingh1911@gmail.com>
2025-08-31 00:43:48 +0530

    Updated TwoSumII

--- Arrays/TwoSumII/TwoSumII.cpp
+++ Arrays/TwoSumII/TwoSumII.cpp
@@ -8,17 +8,20 @@ public:
     {
         int n = arr.size();
         int left = 0, right = n - 1;
-        sort(arr.begin(), arr.end());
+        sort(arr.begin(), arr.end()); // sort array first
+
         while (left < right)
         {
-            int sum = arr[left] + arr[right];
+            int sum = arr[left] + arr[right]; // current pair sum
+
             if (sum == target)
-                return true;
+                return true; // pair found
             else if (sum < target)
-                left++;
+                left++; // increase sum by moving left pointer
             else
-                right--;
+                right--; // decrease sum by moving right pointer
         }
-        return false;
+
+        return false; // no pair found
     }
 };
