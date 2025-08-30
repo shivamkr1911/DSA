7cefb351d166dba46aaa851b0ad5c71263b177a7
Shivam <shivamkrsingh1911@gmail.com>
2025-08-31 02:41:26 +0530

    Updated MaximumSubarraySum-Kadane'sAlgorithm

--- Arrays/MaximumSubarraySum-Kadane'sAlgorithm/MaximumSubarraySum-Kadane'sAlgorithm.cpp
+++ Arrays/MaximumSubarraySum-Kadane'sAlgorithm/MaximumSubarraySum-Kadane'sAlgorithm.cpp
@@ -6,16 +6,51 @@ class Solution
 public:
     int maxSubArray(vector<int> &nums)
     {
-        // Kadane's Algorithm
-        int maxi = INT_MIN;
-        int sum = 0;
-        for (int i = 0; i < nums.size(); i++)
+        // Optimal Solution - Kadane's Algorithm
+        // Idea: Keep running sum, reset if sum < 0, track maximum sum
+        int n = nums.size();
+        int sum = 0;        // current subarray sum
+        int maxi = INT_MIN; // maximum sum so far
+
+        for (int i = 0; i < n; ++i)
         {
-            sum += nums[i];
-            maxi = max(sum, maxi);
+            sum += nums[i];        // include current element
+            maxi = max(maxi, sum); // update maximum sum
             if (sum < 0)
-                sum = 0;
+                sum = 0; // reset if sum negative
+        }
+        return maxi; // return maximum subarray sum
+
+        // Better Solution - Prefix Sum Approach
+        /*
+        // Check sum of all subarrays starting from each index
+        int maxi = INT_MIN;
+        int n = nums.size();
+        for (int i = 0; i < n; ++i) {
+            int sum = 0;
+            for (int j = i; j < n; ++j) {
+                sum += nums[j];       // sum from i to j
+                maxi = max(maxi, sum); // update maximum
+            }
         }
         return maxi;
+        */
+
+        // Brute Force Solution - Triple Nested Loops
+        /*
+        // Compute sum for all possible subarrays
+        int maxi = INT_MIN;
+        int n = nums.size();
+        for (int i = 0; i < n; ++i) {
+            for (int j = i; j < n; ++j) {
+                int sum = 0;
+                for (int k = i; k <= j; ++k) {
+                    sum += nums[k];       // sum of subarray nums[i..j]
+                }
+                maxi = max(maxi, sum);   // update maximum sum
+            }
+        }
+        return maxi;
+        */
     }
 };
