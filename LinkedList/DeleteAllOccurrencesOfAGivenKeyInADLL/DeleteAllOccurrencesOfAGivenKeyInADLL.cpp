ffea6b92cc0fbdf0ce9f4cbc0c4b96fd0f40c50f
Shivam <shivamkrsingh1911@gmail.com>
2025-08-31 21:46:35 +0530

    Refactor DeleteAllOccurrencesOfAGivenKeyInADLL

--- LinkedList/DeleteAllOccurrencesOfAGivenKeyInADLL/DeleteAllOccurrencesOfAGivenKeyInADLL.cpp
+++ LinkedList/DeleteAllOccurrencesOfAGivenKeyInADLL/DeleteAllOccurrencesOfAGivenKeyInADLL.cpp
@@ -18,48 +18,36 @@ class Solution
 public:
     void deleteAllOccurOfX(struct Node **head_ref, int x)
     {
-        // Pointer to traverse the list
-        Node *temp = *head_ref;
+        // Start traversing from head
+        Node *temp = (*head_ref);
 
         while (temp != NULL)
         {
             if (temp->data == x)
             { // Found node to delete
-                if (temp == *head_ref)
-                { // Node is head
-                    if ((*head_ref)->next == NULL)
-                    {
-                        // Only one node in list
-                        delete head_ref;
-                        *head_ref = NULL;
-                        return;
-                    }
-                    else
-                    {
-                        // Head has next node
-                        Node *del = temp;
-                        *head_ref = (*head_ref)->next; // move head
-                        (*head_ref)->prev = NULL;      // update prev of new head
-                        temp = *head_ref;              // move temp
-                        delete del;                    // free memory
-                        continue;                      // continue with new head
-                    }
+                Node *nextNode = temp->next;
+                Node *prevNode = temp->prev;
+
+                // Update links of previous and next nodes
+                if (nextNode != NULL)
+                    nextNode->prev = prevNode;
+                if (prevNode != NULL)
+                    prevNode->next = nextNode;
+
+                // If deleting head, update head pointer
+                if (temp == (*head_ref))
+                {
+                    (*head_ref) = nextNode;
                 }
 
-                // Node is in middle or end
-                Node *prevNode = temp->prev;
-                Node *nextNode = temp->next;
-
-                if (prevNode)
-                    prevNode->next = nextNode; // link previous node to next
-                if (nextNode)
-                    nextNode->prev = prevNode; // link next node to previous
-
-                temp = nextNode; // move temp forward
+                // Free memory and move to next node
+                delete temp;
+                temp = nextNode;
             }
             else
             {
-                temp = temp->next; // move to next node
+                // Move forward if no deletion
+                temp = temp->next;
             }
         }
     }
