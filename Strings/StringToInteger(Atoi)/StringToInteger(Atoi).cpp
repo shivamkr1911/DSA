0ff3710c504c44a0365d515117d7c8b259010ab2
Shivam <shivamkrsingh1911@gmail.com>
2025-09-06 00:21:41 +0530

    Added recursive approach

--- Strings/StringToInteger(Atoi)/StringToInteger(Atoi).cpp
+++ Strings/StringToInteger(Atoi)/StringToInteger(Atoi).cpp
@@ -3,9 +3,31 @@ using namespace std;
 
 class Solution
 {
+private:
+    // Recursive helper function to build the integer
+    int helper(const string &s, int i, int sign, long res)
+    {
+        // Base case: stop if out of bounds or non-digit encountered
+        if (i >= s.size() || !isdigit(s[i]))
+            return sign * res;
+
+        int digit = s[i] - '0';
+
+        // Check for overflow before adding the digit
+        if (res > (INT_MAX - digit) / 10)
+            return sign == 1 ? INT_MAX : INT_MIN;
+
+        // Update result
+        res = res * 10 + digit;
+
+        // Recurse for next character
+        return helper(s, i + 1, sign, res);
+    }
+
 public:
     int myAtoi(string s)
     {
+        // Iterative approach
         int n = s.size();
         int res = 0;
         int i = 0;
@@ -37,5 +59,24 @@ public:
 
         // Step 5: Apply sign
         return sign * res;
+
+        // Recursive approach
+        /*
+        int n = s.size();
+        int sign = 1;
+        int i = 0;
+
+        // Skip leading whitespaces
+        while (i < n && s[i] == ' ') i++;
+
+        // Handle sign if present
+        if (i < n && (s[i] == '+' || s[i] == '-')) {
+            sign = (s[i] == '+') ? 1 : -1;
+            i++;
+        }
+
+        // Start recursive conversion
+        return helper(s, i, sign, 0);
+        */
     }
 };
