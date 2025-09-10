294d8e71d85c167bd9e89ba6fbacc8f58826e251
Shivam <shivamkrsingh1911@gmail.com>
2025-09-10 11:47:49 +0530

    Added CountSubsetsWithSumK

---
+++ Recursion/CountSubsetsWithSumK/CountSubsetsWithSumK.cpp
@@ -0,0 +1,35 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+// Recursive function to count subsets with sum = k
+int func(int ind, int sum, vector<int> &arr, int k, int n)
+{
+    if (sum > k)
+        return 0; // pruning: if sum exceeds k, no valid subset
+
+    // base case: reached end of array
+    if (ind == n)
+    {
+        if (sum == k)
+            return 1; // valid subset found
+        else
+            return 0; // not valid
+    }
+
+    // include current element
+    sum += arr[ind];
+    int l = func(ind + 1, sum, arr, k, n);
+
+    // exclude current element (backtrack sum)
+    sum -= arr[ind];
+    int r = func(ind + 1, sum, arr, k, n);
+
+    return l + r; // total ways = include + exclude
+}
+
+// Wrapper function
+int findWays(vector<int> &arr, int k)
+{
+    int n = arr.size();
+    return func(0, 0, arr, k, n); // start recursion from index 0 with sum = 0
+}
---
+++ Recursion/CountSubsetsWithSumK/README.md
@@ -0,0 +1,29 @@
+# Count Subsets with Sum K
+
+You are given an array **arr** of size **n** containing positive integers and a target sum **k**.
+
+Find the number of ways of selecting the elements from the array such that the sum of chosen elements is equal to the target **k**.
+
+Since the number of ways can be very large, print it modulo **10^9 + 7**.
+
+---
+
+### Example:
+
+```
+Input:
+arr = [1, 1, 4, 5]
+
+Output:
+3
+```
+
+**Explanation:**  
+The possible ways are:
+
+- [1, 4]
+- [1, 4]
+- [5]
+
+Hence the output will be **3**.  
+Please note that both 1s present in **arr** are treated differently.
