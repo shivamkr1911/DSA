5c4865b7e1aa0ff1bf749f825bf30654041f02dc
Shivam <shivamkrsingh1911@gmail.com>
2025-10-20 23:39:42 +0530

    Added FinalValueOfVariableAfterPerformingOperations

---
+++ Arrays/FinalValueOfVariableAfterPerformingOperations/FinalValueOfVariableAfterPerformingOperations.cpp
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution
+{
+public:
+    int finalValueAfterOperations(vector<string> &operations)
+    {
+        int ans = 0;
+        for (string op : operations)
+        {
+            if (op == "++X" || op == "X++")
+                ans++;
+            else
+                ans--;
+        }
+        return ans;
+    }
+};
